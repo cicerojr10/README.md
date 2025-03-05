6aa9aab5e642a033d11f64fd320d8b20eb0bc734
Cícero <cicerojr10@gmail.com>
2025-03-05 02:57:04 +0000

    primeira tarefa

---
+++ concat_dados.py
@@ -0,0 +1,10 @@
+# concat_dados.py
+
+def concatena_dados():
+    dado1 = input("Digite o primeiro dado: ")
+    dado2 = input("Digite o segundo dado: ")
+    resultado = dado1 + dado2
+    print("Resultado da concatenação:", resultado)
+
+if __name__ == "__main__":
+    concatena_dados()
